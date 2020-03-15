46579bca8ab70a71415e38779db9ee9e83bb80d8
Giacomo Lorenzo <glorenzo@imolainformatica.it>
2020-03-15 22:24:23 +0100

    client react first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Order from './components/Order';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="App">
+        <div className="container-fluid">
+              <div className='form-row'>
+                  <div className='form-group col mt-2'>
+                    <label for='telefono' className='font-weight-bold'>Nome</label>  
+                  </div>
+                  <div className='form-group col mt-2'>
+                    <label for='telefono' className='font-weight-bold'>Telefono</label>  
+                  </div>
+                  <div className='form-group col mt-2'>
+                    <label for='telefono' className='font-weight-bold'>Ordine</label>  
+                  </div>
+                  <div className='form-group col mt-2'>
+                    <label for='telefono' className='font-weight-bold'>Stato</label>  
+                  </div>
+                  <div className='form-group col mt-2'>
+                    <label for='telefono' className='font-weight-bold'>Prezzo</label>  
+                  </div>
+                </div>
+                <Order></Order>
+          </div>
+      </div>
   );
 }
 
---
+++ src/components/Order.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+class Order extends Component{
+    constructor(props){
+        super(props);
+        this.state={apiResponse:"",
+                    jsonDatas: "",
+                    value: '?'};
+    }
+
+    componentDidMount(){
+        this.callAPI();
+    }
+
+callAPI(){
+     fetch("http://localhost:5000/orders")
+    .then(response => response.json())
+  .then((jsonData) => {
+      const items = [];
+    // jsonData is parsed json object received from url
+    console.log(jsonData)
+
+  for (const [index, value] of jsonData.entries()) {
+    items.push(
+        <div className='form-row' id={value._id}>
+            <div className='form-group col mt-6'>
+             <input type='text' id={'name_'+value._id} name='name' value={value.user} readOnly  className='form-control shadow p-1  bg-white rounded' />
+            </div>
+            <div className='form-group col mt-6'>
+             <input type='text' id={'telefono_'+value._id}  name='telefono' value={value.phone} readOnly  className='form-control shadow p-1  bg-white rounded' />
+            </div>
+            
+            <div className='form-group col mt-6'>
+             <input type='text' id={'ordine_'+value._id} name='ordine' value={value.message} readOnly className='form-control shadow p-1  bg-white rounded' />
+            </div>
+            
+            <div className='form-group col mt-6'>
+             <select id={'selection_'+value._id} name='stato' className='form-control shadow p-1  bg-white rounded' onChange={this.handleChange} >
+                <option value="Aperto" selected>Aperto</option>
+                <option value="Gestito">Gestito</option>
+                <option value="Chiuso">Chiuso</option>
+                <option value ="Pagato">Pagato</option>
+             </select>
+            </div>
+            
+            <div className='form-group col mt-6'>
+             <input type='text'  id={'prezzo_'+value._id} name='prezzo' value={value.prezzo}  className='form-control shadow p-1  bg-white rounded' />
+            </div>
+            </div>
+    )
+    }
+    this.setState({apiResponse:items});
+    this.setState({jsonDatas:jsonData});
+  })
+  .catch((error) => {
+    // handle your errors here
+    console.error(error)
+  })
+}
+handleChange = (event) => {
+    console.log(event.target.value+ " " +event.target.id)
+    this.setState({ value: event.target.value });
+  };
+render(){
+    return (
+        <div>
+            {this.state.apiResponse}
+        </div>
+    )
+}
+}
+
+export default Order;
